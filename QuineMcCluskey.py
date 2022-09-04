f1c6761e17f9f1b4d3f893211b643e3aa0cc5d84
dafta31 <110066350+dafta31@users.noreply.github.com>
2022-09-04 04:08:27 +0000

    Update QuineMcCluskey.py

--- QuineMcCluskey.py
+++ QuineMcCluskey.py
@@ -36,6 +36,41 @@ def leer_minterminos(nombre_archivo):
     minterminos_numeros = list(map(int, minterminos_strings))
     return minterminos_numeros
 
+def partirListaEnDos(lista):
+    '''
+    Resumen: parte una lista en dos.
+
+    Entradas:
+    - lista(list): lista de numeros enteros. Ejemplo: [1,3,5,7]
+
+    Salida:
+    - primerMitad, segundaMitad (tuple): una tupla de las dos mitades. Ejemplo: ([1,3] , [5,7])
+    '''
+    halfpoint = len(lista)/2
+    halfpoint = int(halfpoint)
+
+    primerMitad = lista[:halfpoint]
+    segundaMitad = lista[halfpoint:]
+    return primerMitad, segundaMitad
+
+def implicanteRepetido(numBits, implicante, lista):
+    '''
+    Resumen: verifica si un implicante primo se encuentra en la lista pero en diferente orden.
+
+    Entradas:
+    - numBits(int): numero de bits de la expresion booleana. Ejemplo: ABC || BC'D -> numBits es 4
+    - implicante(list): el correspondiente implicante a comprobar. Ejemplo: [1,5,3,7]
+    - lista(list): lista de listas de implicantes primos. Ejemplo: [[10,11], [1,3,5,7]]
+
+    Salida:
+    - (bool) retorna true si sí se encuentra. Ejemplo: [1,5,3,7] -> [[10,11], [1,3,5,7]] = True
+    '''
+    a = combinarMin(numBits, implicante)
+    for k in lista:
+        if a == combinarMin(numBits, k):
+            return True
+    return False
+
 def convertMinTermABinario(numBits, decMinTerm):
     """
     Resumen: convierte una lista de minterminos en decimal a un diccionario con representacion binaria
@@ -210,11 +245,11 @@ def hallarPrimos(numBits, listaInicial, listaPrimos, primerCorrida):
 
     Entrada:
     - numBits(int): numero de bits de la expresion booleana. Ejemplo: ABC || BC'D -> numBits es 4
-    - listaInicial(list): una lista de listas de mintérminos. Ejemplo:[[1],[3],[5],[7]]
+    - listaInicial(list): una lista de listas de mintérminos. Ejemplo: [[1],[3],[5],[7]]
     - listaPrimos(list): aquí almacena los implicantes primos que encuentra.
 
     Salida:
-    - (list) la lista de implicantes primos. Ejemplo: [1,3,5,7]
+    - (list) la lista de implicantes primos. Ejemplo: [[1,3,5,7]]
     
     '''
     paresAgrupados = list()
@@ -236,10 +271,10 @@ def hallarPrimos(numBits, listaInicial, listaPrimos, primerCorrida):
                             combinarMinTerDecBin(numBits, listaInicial[j][0])) == True:
                             paresAgrupados.append([listaInicial[i][0], listaInicial[j][0]])
                             huboCombinacion = True
-
-                            if implicantesUtilizados.count(listaInicial[i]) < 0:
+                            if implicantesUtilizados.count(listaInicial[i]) == 0:
+                                
                                 implicantesUtilizados.append(listaInicial[i])
-                            if implicantesUtilizados.count(listaInicial[j]) < 0:
+                            if implicantesUtilizados.count(listaInicial[j]) == 0:
                                 implicantesUtilizados.append(listaInicial[j])                                
 
                     elif j > i:
@@ -248,13 +283,13 @@ def hallarPrimos(numBits, listaInicial, listaPrimos, primerCorrida):
                             paresAgrupados.append(listaInicial[i] + listaInicial[j])
                             huboCombinacion = True
 
-                            if implicantesUtilizados.count(listaInicial[i]) < 0:
+                            if implicantesUtilizados.count(listaInicial[i]) == 0:
                                 implicantesUtilizados.append(listaInicial[i])
-                            if implicantesUtilizados.count(listaInicial[j]) < 0:
+                            if implicantesUtilizados.count(listaInicial[j]) == 0:
                                 implicantesUtilizados.append(listaInicial[j])   
 
     for k in listaInicial:
-        if implicantesUtilizados.count(k) < 0:
+        if implicantesUtilizados.count(k) == 0 and implicanteRepetido(numBits, k ,listaPrimos) == False:
             listaPrimos.append(k)
 
     if huboCombinacion == False: #No hubo combinaciones, no repita más
@@ -264,9 +299,23 @@ def hallarPrimos(numBits, listaInicial, listaPrimos, primerCorrida):
         primerCorrida = False
 
         #Con los elementos que mezclo vuelva a hacer el mismo procedimiento hasta que no se puedan mezclar más
-        hallarPrimos(numBits, paresAgrupados, listaPrimos, primerCorrida)
-        
-   def representarBinario(impPrimo, listaInicialMin):
+        return hallarPrimos(numBits, paresAgrupados, listaPrimos, primerCorrida)
+
+
+def Petricks():
+    '''
+    Resumen:
+
+    Entradas:
+    - listaPrimos(list): lista implicantes primos. Ejemplo: [[3,7,11,15], [1,3,9,11], [1,3,5,7], [10,11], [4,5]]
+
+    Salidas:
+    - listaEsenciales(list): lista de los implicantes esenciales. Ejemplo: [[3,7,11,15], [1,3,9,11], [10,11], [4,5]]
+
+    Todos los mintermnos : 1,3,4,5,7,9,10,11,15                      
+    '''
+
+def representarBinario(impPrimo, listaInicialMin):
     '''
     Resumen: los números que la lista no contiene son 0's, aquellos que sí tiene son 1's
 
