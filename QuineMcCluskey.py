d0f511a4c51b9e7b9230ed5aee937408ad0465b0
dafta31 <110066350+dafta31@users.noreply.github.com>
2022-09-05 19:58:02 +0000

    Update QuineMcCluskey.py

--- QuineMcCluskey.py
+++ QuineMcCluskey.py
@@ -25,33 +25,62 @@ import itertools
 
 #Los implicantes primos esenciales corresponden a la ecuación booleana reducida
 
-def leer_minterminos(nombre_archivo):
-    # directorio del archivo
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    archivo_abierto = open(nombre_archivo,"r")
-    linea = archivo_abierto.readline()
-    # los minterminos se leen como un string '1,2,4,5',
-    # Por lo que hay que convertirlos en una lista de numeros 
-    minterminos_strings = linea.split(',')
-    minterminos_numeros = list(map(int, minterminos_strings))
-    return minterminos_numeros
+import argparse
+from distutils.log import info
+import sys
 
-def partirListaEnDos(lista):
+def representarBinario(impPrimo, listaInicialMin):
     '''
-    Resumen: parte una lista en dos.
+    Resumen: los números que la lista no contiene son 0's, aquellos que sí tiene son 1's
+    Entradas:
+    - impPrimo(list): lista de numeros enteros. Ejemplo: [3,7,11,15]
+    - listaInicialMin(list): lista incial de minterminos. Ejemplo: [1,3,4,5,7,9,10,11,15]
+    Salida:
+    - respuesta(str): Ejemplo: '010010011'
+    '''
+    #repreBits me dira si un elemento es repetido o no, sera util en petrick para identificar implicantes primos esenciales
+    repreBits= str()
+    for i in range(0,len(listaInicialMin)):     
+        if impPrimo.count(listaInicialMin[i])!=0:
+            repreBits+= '1' #si el elemento se repite, se identifica con 1
+        else:
+            repreBits+= '0'#si el elemento no se repite, se identifica con 0        
+    return repreBits
+
+def Petricks(listaInicialMin, listaPrimos):
+    '''
+    Resumen: encuentra los implicantes esenciales
 
     Entradas:
-    - lista(list): lista de numeros enteros. Ejemplo: [1,3,5,7]
+    - listaPrimos(list): lista de los implicantes primos.
+    Ejemplo: [[4, 5], [10, 11], [1, 3, 5, 7], [1, 3, 9, 11], [3, 7, 11, 15]]
+    - listaInicialMin(list): lista incial de minterminos.
+    Ejemplo: [1,3,4,5,7,9,10,11,15]
+    
 
-    Salida:
-    - primerMitad, segundaMitad (tuple): una tupla de las dos mitades. Ejemplo: ([1,3] , [5,7])
+    Salidas:
+    - listaEsenciales(list): lista de los implicantes esenciales. 
+    Ejemplo: [[4, 5], [10, 11], [1, 3, 9, 11], [3, 7, 11, 15]]              
     '''
-    halfpoint = len(lista)/2
-    halfpoint = int(halfpoint)
+    listaPrimoBits = list()
+    listaEsenciales = list()
 
-    primerMitad = lista[:halfpoint]
-    segundaMitad = lista[halfpoint:]
-    return primerMitad, segundaMitad
+    #Añade los implicantes primos a una lista en una representación en Bits.
+    for i in range(0, len(listaPrimos)):
+        listaPrimoBits.append(representarBits(listaPrimos[i],listaInicialMin))
+
+    for j in range(0, len(listaPrimos)):
+        for k in range(0, len(listaInicialMin)): #Itera cifra por cifra
+            unico = True
+            #Si dos cifras coinciden entonces el implicante no es esencial
+            for z in range(0, len(listaPrimos)):
+                if j != z and listaPrimoBits[j][k] == listaPrimoBits[z][k]:
+                    unico = False
+            #Si en ninguna cifra coincida anadala a la lista de esenciales
+            if unico == True:
+                listaEsenciales.append(listaPrimos[j])
+
+    return listaEsenciales
 
 def implicanteRepetido(numBits, implicante, lista):
     '''
@@ -70,104 +99,6 @@ def implicanteRepetido(numBits, implicante, lista):
         if a == combinarMin(numBits, k):
             return True
     return False
-
-def convertMinTermABinario(numBits, decMinTerm):
-    """
-    Resumen: convierte una lista de minterminos en decimal a un diccionario con representacion binaria
-
-    Entradas
-    - numBits: numero de bits de la expresion booleana. Ejemplo: ABC || BC'D -> numBits es 4
-    - decMinTerm: lista con minterminos en decimal. Ejemplo: [1,3,5,7]
-
-    Salidas:
-    - un diccionario donde cada pareja es la representacion decimal y binaria del mintermino
-    Ejemplo: { 1: '0001', 3: '0011', 5:'0101', 7:'0111'} -> numBits es 4
-    """
-    diccionario = dict() #Crea un diccionario vacio
-    minTerBin = str()    #Crea una cadena vacia
-
-    for i in decMinTerm:
-        minTerBin = bin(i)  #Convierte de dec a bin: bin(11) -> '0b1011'
-        minTerBin = minTerBin[2:] #Trunca el '0b' para que quede solo '1011'
-
-        #Anade ceros segun el numero de bits. Por ejemplo 8 bits es '00001011'
-        diccionario[i]= minTerBin.zfill(numBits) 
-    return diccionario
-
-def agruparPorUnos(numBits, minTerSinAgrupar):
-    """
-    Resumen: agrupa los minterminos segun el numero de 1's
-
-    Entradas
-    - numBits: numero de bits de la expresion booleana. Ejemplo: ABC || BC'D -> numBits es 4
-    - minTerm: diccionario con minterminos sin agrupar. Ejemplo: {1:'0001', 2:'0010', 3:'0011', 7:'0111', 8:'1000', 15:'1111'}
-
-    Salidas:
-    - una lista de diccionarios con los minterminos agrupados. El indice equivale al num de 1's del grupo
-    Ejemplo: [{}, {1: '0001', 2: '0010', 8: '1000'}, {3: '0011'}, {7: '0111'}] -> numBits es 4
-    """
-    minTerAgrupados = list()
-
-    for i in range(0, numBits):         #i es el numero de 1's a buscar.
-        diccionario = dict()            #Cree un diccionario vacio
-        for key in minTerSinAgrupar:    #Itere por el diccionario desagrupado en busqueda de un mintermino con i numero de 1's
-            if minTerSinAgrupar[key].count('1') == i:    
-                diccionario[key] = minTerSinAgrupar[key] # Si coincide anadalo al diccionario
-
-        minTerAgrupados.append(diccionario)  #Agregue el diccionario a la lista
-    return minTerAgrupados
-
-def difierenUnaCifra(numBits, minTer1, minTer2):
-    """
-    Resumen: comprueba si un mintermino difiere en una cifra.
-
-    Entradas
-    - numBits: numero de bits de la expresion booleana. Ejemplo: ABC || BC'D -> numBits es 4
-    - minTer1 y minTer2: las cadenas de minterminos a comparar. Ejemplo: minTer1-> '1100' & minTer2 -> '1111'
-
-    Salidas:
-    - si difieren en una cifra retorna true. De lo contrario false. (Si difieren en X's retorna false)
-    Ejemplo: '1101' y '1111' -> True
-             '1100' y '1111' -> False
-             '1XX1' y '10X1' -> False
-    """
-    cifrasDiferidas = 0
-
-    for i in range(0, numBits):
-        if (minTer1[i] != minTer2[i]) and (minTer1[i] == 'X'): #Pueden distinguir en 1's pero no en X's
-            return False
-        elif minTer1[i] != minTer2[i]: # Compare si difieren en una cifra
-            cifrasDiferidas += 1
-        else:
-            continue
-    if cifrasDiferidas > 1: #Si difieren en mas de una cifra entonces no
-        return False
-    else:
-        return True
-
-def imprimirImpEsencialesLit(numBits, impEsenBin):
-    '''
-    Resumen: imprime los minterminos esenciales en su forma literal
-
-    Entrada: lista de minterminos mezclados en su forma binaria. Ejemplo: ['1XX1','0X1X','111X']
-
-    Salida: la cadena con los minterminos en su forma literal. Ejemplo: ' AD + A*C + ABC '
-    '''
-    cadena = 'ABCDEF'
-    impEsenLit = str()
-
-    for i in range(0, len(impEsenBin)): #i representa los indices de la lista
-        for j in range(0, numBits): #j representa los indices de las cadenas
-            if impEsenBin[i][j] == '1': 
-                impEsenLit += cadena[j]
-            elif impEsenBin[i][j] == '0':
-                impEsenLit += cadena[j] + '*'
-            else:
-                impEsenBin[i][j] == 'X'
-        if i != len(impEsenBin)-1:
-            impEsenLit += ' + '
-    return impEsenLit
-
 
 def mezclarMinTer(numBits, minTer1, minTer2):
     '''
@@ -190,35 +121,22 @@ def mezclarMinTer(numBits, minTer1, minTer2):
             resultado += minTer1[i]
     return resultado
 
-def convertirBinarioALiteral(numBits, minTerBinario):
+def partirListaEnDos(lista):
     '''
-    Resumen: 
+    Resumen: parte una lista en dos.
 
     Entradas:
-    -num bits: numero de bits de la expresion booleana. Ejemplo: ABC || BC'D -> numBits es 4
+    - lista(list): lista de numeros enteros. Ejemplo: [1,3,5,7]
 
-    Salidas:
+    Salida:
+    - primerMitad, segundaMitad (tuple): una tupla de las dos mitades. Ejemplo: ([1,3] , [5,7])
     '''
-    cadena = 'ABCDEF'
-    resultado = str()
+    halfpoint = len(lista)/2
+    halfpoint = int(halfpoint)
 
-    for i in range(0, numBits):
-
-        if minTerBinario[i] == '1':  
-            resultado += cadena[i]  
-        elif minTerBinario[i] == '0':
-            resultado += cadena[i] + '*'
-        else:
-            pass
-    return resultado
-
-def combinarMinTerDecBin(numBits, num):
-    cadena = str()
-    cadena = bin(num)  #Convierte de dec a bin: bin(11) -> '0b1011'
-    cadena = cadena[2:] #Trunca el '0b' para que quede solo '1011'
-
-    #Anade ceros segun el numero de bits. Por ejemplo 8 bits es '00001011'
-    return cadena.zfill(numBits) 
+    primerMitad = lista[:halfpoint]
+    segundaMitad = lista[halfpoint:]
+    return primerMitad, segundaMitad
 
 def combinarMin(numBits, secuencia):
     '''
@@ -231,25 +149,81 @@ def combinarMin(numBits, secuencia):
     - (str) la representación que se consigue dada dicha combinación. Ejemplo: [1,3,5,7] -> [1,3] y [5,7]. 
                                                                                 Entonces '00X1' y '01X1'
                                                                                 Finalmente -> '0XX1'
-    '''                                                                                                  
-    if len(secuencia) != 2:
+    '''
+    if len(secuencia) == 1:
+        return combinarMinTerDecBin(numBits, secuencia[0])                                                                                               
+    elif len(secuencia) != 2:
         a = combinarMin(numBits, partirListaEnDos(secuencia)[0])
         b = combinarMin(numBits, partirListaEnDos(secuencia)[1])
         return mezclarMinTer(numBits, a, b)
 
+
+
     return mezclarMinTer(numBits, combinarMinTerDecBin(numBits, secuencia[0]), combinarMinTerDecBin(numBits, secuencia[1]))
 
-def hallarPrimos(numBits, listaInicial, listaPrimos, primerCorrida):
+def combinarMinTerDecBin(numBits, minDec):
+    '''
+    Resumen: convierte un mintérmino de decimal a binario
+
+    Entadas:
+    - numBits(int): numero de bits de la expresion booleana. Ejemplo: ABC || BC'D -> numBits es 4
+    - minTer(int): el mintérmino en representación decimal
+
+    Salida
+    - (str) retorna una cadena del mintérmino en decimal
+
+    '''
+    cadena = str()
+    cadena = bin(minDec)  #Convierte de dec a bin: bin(11) -> '0b1011'
+    cadena = cadena[2:] #Trunca el '0b' para que quede solo '1011'
+
+    #Anade ceros segun el numero de bits. Por ejemplo 8 bits es '00001011'
+    return cadena.zfill(numBits)
+
+def difierenUnaCifra(numBits, minTer1, minTer2):
+    """
+    Resumen: comprueba si un mintermino difiere en una cifra.
+
+    Entradas
+    - numBits(int): numero de bits de la expresion booleana. Ejemplo: ABC || BC'D -> numBits es 4
+    - minTer1(str) y minTer2(str): las cadenas de minterminos a comparar. Ejemplo: minTer1-> '1100' & minTer2 -> '1111'
+
+    Salidas:
+    - (bool) si difieren en una cifra retorna true. De lo contrario false. (Si difieren en X's retorna false)
+    Ejemplo: '1101' y '1111' -> True
+             '1100' y '1111' -> False
+             '1XX1' y '10X1' -> False
+    """
+    cifrasDiferidas = 0
+
+    for i in range(0, numBits):
+        if (minTer1[i] != minTer2[i]) and (minTer1[i] == 'X'): #Pueden distinguir en 1's pero no en X's
+            return False
+        elif minTer1[i] != minTer2[i]: # Compare si difieren en una cifra
+            cifrasDiferidas += 1
+        else:
+            continue
+    if cifrasDiferidas == 0: #Si difieren en mas de una cifra entonces no
+        return False
+    elif cifrasDiferidas > 1:
+        return False
+    else:
+        return True
+
+def hallarPrimos( numBits, listaInicial, listaPrimos, primerCorrida):
     '''
     Resumen: la función recursiva combina los mintérminos y finalmente halla los primos.
 
     Entrada:
     - numBits(int): numero de bits de la expresion booleana. Ejemplo: ABC || BC'D -> numBits es 4
-    - listaInicial(list): una lista de listas de mintérminos. Ejemplo: [[1],[3],[5],[7]]
-    - listaPrimos(list): aquí almacena los implicantes primos que encuentra.
+    - listaInicial(list): una lista de listas de mintérminos. 
+    Ejemplo: [[1],[3],[4],[5],[7],[9],[10],[11],[15]]
+    - listaPrimos(list): aquí almacena los implicantes primos que encuentra. Comienza vacía
+    Ejemplo: []
 
     Salida:
-    - (list) la lista de implicantes primos. Ejemplo: [[1,3,5,7]]
+    - (list) la lista de implicantes primos. 
+    Ejemplo: [[4, 5], [10, 11], [1, 3, 5, 7], [1, 3, 9, 11], [3, 7, 11, 15]]
     
     '''
     paresAgrupados = list()
@@ -289,8 +263,11 @@ def hallarPrimos(numBits, listaInicial, listaPrimos, primerCorrida):
                                 implicantesUtilizados.append(listaInicial[j])   
 
     for k in listaInicial:
-        if implicantesUtilizados.count(k) == 0 and implicanteRepetido(numBits, k ,listaPrimos) == False:
-            listaPrimos.append(k)
+        if implicantesUtilizados.count(k) == 0:
+            if primerCorrida == False and implicanteRepetido(numBits, k ,listaPrimos) == False:
+                listaPrimos.append(k)
+            if primerCorrida == True:
+                listaPrimos.append(k)
 
     if huboCombinacion == False: #No hubo combinaciones, no repita más
         return listaPrimos
@@ -301,66 +278,16 @@ def hallarPrimos(numBits, listaInicial, listaPrimos, primerCorrida):
         #Con los elementos que mezclo vuelva a hacer el mismo procedimiento hasta que no se puedan mezclar más
         return hallarPrimos(numBits, paresAgrupados, listaPrimos, primerCorrida)
 
-def representarBinario(impPrimo, listaInicialMin):
-    '''
-    Resumen: los números que la lista no contiene son 0's, aquellos que sí tiene son 1's
+def leer_minterminos(nombre_archivo):
+    # directorio del archivo
+    archivo_abierto = open(nombre_archivo,"r")
+    linea = archivo_abierto.readline()
+    # los minterminos se leen como un string '1,2,4,5',
+    # Por lo que hay que convertirlos en una lista de numeros 
+    minterminos_strings = linea.split(',')
+    minterminos_numeros = list(map(int, minterminos_strings))
+    return minterminos_numeros
 
-    Entradas:
-    - impPrimo(list): lista de numeros enteros. Ejemplo: [3,7,11,15]
-    - listaInicialMin(list): lista incial de minterminos. Ejemplo: [1,3,4,5,7,9,10,11,15]
-
-    Salida:
-    - respuesta(str): Ejemplo: '010010011'
-    '''
-    #repreBits me dira si un elemento es repetido o no, sera util en petrick para identificar implicantes primos esenciales
-    repreBits= str()
-    for i in range(0,len(listaInicialMin)):     
-        if impPrimo.count(listaInicialMin[i])!=0:
-            repreBits+= '1' #si el elemento se repite, se identifica con 1
-        else:
-            repreBits+= '0'#si el elemento no se repite, se identifica con 0
-            
-                
-    return repreBits
-    
-    
-    
-    
- def Petricks(listaInicialMin, listaPrimos):
-    '''
-    Resumen: encuentra los implicantes esenciales
-
-    Entradas:
-    - listaPrimos(list): lista de los implicantes primos.
-    Ejemplo: [[4, 5], [10, 11], [1, 3, 5, 7], [1, 3, 9, 11], [3, 7, 11, 15]]
-    - listaInicialMin(list): lista incial de minterminos.
-    Ejemplo: [1,3,4,5,7,9,10,11,15]
-    
-
-    Salidas:
-    - listaEsenciales(list): lista de los implicantes esenciales. 
-    Ejemplo: [[4, 5], [10, 11], [1, 3, 9, 11], [3, 7, 11, 15]]              
-    '''
-    listaPrimoBits = list()
-    listaEsenciales = list()
-
-    #Añade los implicantes primos a una lista en una representación en Bits.
-    for i in range(0, len(listaPrimos)):
-        listaPrimoBits.append(representarBits(listaPrimos[i],listaInicialMin))
-
-    for j in range(0, len(listaPrimos)):
-        for k in range(0, len(listaInicialMin)): #Itera cifra por cifra
-            unico = True
-            #Si dos cifras coinciden entonces el implicante no es esencial
-            for z in range(0, len(listaPrimos)):
-                if j != z and listaPrimoBits[j][k] == listaPrimoBits[z][k]:
-                    unico = False
-            #Si en ninguna cifra coincida anadala a la lista de esenciales
-            if unico == True:
-                listaEsenciales.append(listaPrimos[j])
-
-    return listaEsenciales
-    
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output", help="Nombre del archivo de salida")
@@ -380,7 +307,16 @@ def main():
     nombre_archivo_salida = args.output 
 
     minterminos = leer_minterminos(nombre_archivo_minterminos)
-    print(minterminos)
 
+    numeroBits = (max(minterminos)).bit_length() # Según el mintérmino más grande, coloca el mínimo num de bits necesarios
 
+    for i in range(0, len(minterminos)): #Convierte [1,4,6,15] a -> [[1], [4], [6], [15]].                       
+        resultado = list()               #Esto porque la funcion hallarPrimos los acepta así
+        resultado.append(minterminos[i])
+        minterminos[i] = resultado
 
+    return numeroBits, minterminos
+
+a = main()
+implicantesPrimos = hallarPrimos(a[0], a[1], [], True)
+print( Petricks(listaInicialMin, implicantesPrimos) )
